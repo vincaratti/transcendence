4e2e98e139121fbea1c989d6f6b0272cf575a604
Pierre-YvesRaucq <py@raucq-musick.be>
2026-07-15 17:06:39 +0200

    GodotGame errors and warning fixing
    Should fix most if not all current errors and warnings.

--- GodotGame/game.service.worker.js
+++ GodotGame/game.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783671817|4733731654';
+const CACHE_VERSION = '1784127523|1471151671';
 /** @type {string} */
 const CACHE_PREFIX = 'Godot_first_game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
