5906a5db602ec4a6bf697e2a2ca5de5b31a807cd
Pierre-YvesRaucq <py@raucq-musick.be>
2026-07-16 11:45:56 +0200

    Forgot a underscore, which leads to an warning/error, fixed. Plus attempt at adding a favico

--- GodotGame/game.service.worker.js
+++ GodotGame/game.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784194661|4456001944';
+const CACHE_VERSION = '1784194829|4624385417';
 /** @type {string} */
 const CACHE_PREFIX = 'Godot_first_game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
